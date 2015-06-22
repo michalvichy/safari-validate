32a3049ce111bd5a17db6e637b31a3c50bb3cc7c
mskora <michalvichy@gmail.com>
2015-06-22 23:07:42 +0200

    Form validate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,60 @@
-(function(){
+$.fn.validate = function() {
+    var $this = $(this);
+    var pattern = new RegExp($this.attr('pattern'));
 
+    return $this.keyup(function(){
+        if($this.val().trim().match(pattern)) {
+            $this.valid();
+        } else {
+            $this.invalid();
+        }
+
+        return this;
+    });
+
+};
+
+$.fn.validateForm = function() {
+    // FIXME what if input does not have patter?
+    $inputs = this.find('input[type!="submit"]');
+
+    $inputs.each(function() {
+        $(this).validate();
+    });
+};
+
+$.fn.disable = function() {
+    this.attr('disabled', true);
+
+    return this;
+};
+
+$.fn.enable = function() {
+    this.attr('disabled', false);
+
+    return this;
+};
+
+$.fn.valid = function() {
+    this.removeClass('invalid').addClass('valid');
+
+    return this;
+};
+
+$.fn.invalid = function() {
+    this.removeClass('valid').addClass('invalid');
+
+    return this;
+};
+
+String.prototype.contain = function(string) {
+    return this.indexOf(string) !== -1;
+};
+
+(function(){
+    "use strict";
+
+    if(navigator.appName.contain('Safari')) {
+        $('.validate-form').validateForm();
+    }
 })();
